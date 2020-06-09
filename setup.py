25024e0776fc59e7738a56bcc163c22653993c9d
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-06-09 09:44:41 +0200

    renamed

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     long_description=readme,
     include_package_data=True,
     keywords='python_shell_colors',
-    name='python_shell_colors',
+    name='python-shell-colors',
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
