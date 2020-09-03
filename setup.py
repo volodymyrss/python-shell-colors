58031650d18745261261ea4769309a3a0a7523d5
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-09-03 16:52:17 +0200

    Bump version: 0.1.10-dev0 → 0.1.10

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/volodymyrss/python-shell-colors',
-    version='0.1.10-dev0',
+    version='0.1.10',
     zip_safe=False,
 )
